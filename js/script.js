27593251c83338dd86d7fab58b7ae00be25a6919
nisingizwe-joselyne <nesjoselyne@gmail.com>
2020-10-01 12:16:14 +0200

    files made it well

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
 function validate() {
   var genders = document.getElementsByName("gender");
-  if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1900) {
+  if( document.myForm.year.value == "" && document.myForm.year.value.length !=4 && document.myForm.year.value >2100 && document.myForm.year.value <=1900) {
      alert( "Please provide a valid year of birth! eg 2019" );
      document.myForm.year.focus() ;
      return false;
   }
-  else if( document.myForm.month.value == "" || isNaN( document.myForm.month.value ) || 
-  document.myForm.month.value.length != 2 || document.myForm.month.value > 12  || document.myForm.month.value <= 0){
+  else if( document.myForm.month.value == "" && isNaN( document.myForm.month.value ) && 
+  document.myForm.month.value.length != 2 && document.myForm.month.value > 12  && document.myForm.month.value <= 0){
      alert( "Please provide your month of birth! between 1 and 12" );
      document.myForm.month.focus() ;
      return false;
   }
-  else if( document.myForm.date.value == "" || isNaN( document.myForm.month.value ) || 
-  document.myForm.month.value.length != 2|| document.myForm.date.value > 31 || document.myForm.date.value <= 0) {
+  else if( document.myForm.date.value == "" && isNaN( document.myForm.month.value ) || 
+  document.myForm.month.value.length != 2 && document.myForm.date.value > 31 && document.myForm.date.value <= 0) {
      alert( "Please provide a valid date that you were born in!" );
      document.myForm.day.focus() ;
      return false;
